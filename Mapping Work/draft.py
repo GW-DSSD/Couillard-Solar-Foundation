0a08fb65bf933a76fd921c8e31e80ea4ce274e0b
Apoorva Reddy <61325120+apoorvareddy612@users.noreply.github.com>
2023-11-11 16:16:04 -0500

    update draft
    with shaded country except Wisconsin state

--- Mapping Work/draft.py
+++ Mapping Work/draft.py
@@ -3,10 +3,10 @@ import pandas as pd
 from folium.plugins import Geocoder
 
 #Read csv file containing coordinates and other factors/features
-data = pd.read_csv('Dataset/data.csv').drop(['Unnamed: 0'],axis=1)
+data = pd.read_csv('Couillard-Solar-Foundation/Dataset/data.csv').drop(['Unnamed: 0'],axis=1)
 
 #Intiate a default map 
-usa = folium.Map([43.7844,-88.7879],zoom_start=7)
+usa = folium.Map([44.5, -89.5],zoom_start=7.4)
 
 
 #Data preprocessing
@@ -63,9 +63,31 @@ for marker_type, icon_params in marker_types.items():
     for idx, row in data[data['Marker_type'] == marker_type].iterrows():
         marker(row, icon_params[0],icon_params[1])
 
+borderStyle = {
+    'color' : 'black',
+    'weight' : 2,
+    'fillcolor' : 'black',
+    'fillOpacity' : 0.2
+
+}
+folium.GeoJson("Couillard-Solar-Foundation/Mapping Work/us-state-boundaries_1.geojson",
+               name="Country",
+               style_function=lambda x:borderStyle).add_to(usa)
+bordersStyle = {
+    'color' : 'white',
+    'weight' : 2,
+    'fillcolor' : 'white',
+    'fillOpacity' : 0
+
+}
+
+folium.GeoJson("Couillard-Solar-Foundation/Mapping Work/us-state-boundaries.geojson",
+               name="Wisconsin",
+               style_function=lambda x:bordersStyle).add_to(usa)
+
 folium.LayerControl().add_to(usa)
 Geocoder().add_to(usa)
-usa.save('Mapping Work/Sample Maps/usa.html')
+usa.save('Couillard-Solar-Foundation/Mapping Work/Sample Maps/usa.html')
 
 
 
@@ -76,64 +98,3 @@ usa.save('Mapping Work/Sample Maps/usa.html')
 
 
 
-# #print(pd.value_counts(data["Marker_type"]))
-# for idx, row in data.iterrows():
-#     row['Value of grant'] = str('{:,}'.format(row['Value of grant']))
-#     if (row['Marker_type'] == 'Education Institutes'):
-#         if(not pd.isnull(row["Image URL"])):
-#             if(row['Installer'] != 'Unknown'):
-#                 test = folium.Html('<center><img src=' + row["Image URL"] + ' alt="logo" width=100 height=100 ></center>'+'<h5>'+row["Recipient"]+'</h5>'+'<p>('+row['Type']+')</p>'+'<b> Project Grant : $' +str(row['Value of grant'])+ '</b> <br>'+'<b> Array Size : '+str(row['Size of Array (in kW)'])+'</b> <br>'+'<b> Installer :'+row['Installer']+'</b>', script=True)
-#                 popup = folium.Popup(test, max_width=2650)
-#                 folium.Marker(location=[row["Latitude"], row["Longitude"]],popup=popup,icon=folium.Icon(color='red',icon='university', prefix='fa')).add_to(group_1)
-#             else:
-#                 test = folium.Html('<h5>'+row["Recipient"]+'</h5>'+'<p>('+row['Type']+')</p>'+'<b> Project Grant : $' +str(row['Value of grant'])+ '</b> <br>'+'<b> Array Size : '+str(row['Size of Array (in kW)'])+'</b>', script=True)
-#                 popup = folium.Popup(test, max_width=2650)
-#                 folium.Marker(location=[row["Latitude"], row["Longitude"]],popup=popup,icon=folium.Icon(color='red',icon='university', prefix='fa')).add_to(group_1)
-#         else:
-#             if(row['Installer'] != 'Unknown'):
-#                 test = folium.Html('<h5>'+row["Recipient"]+'</h5>'+'<p>('+row['Type']+')</p>'+'<b> Project Grant : $' +str(row['Value of grant'])+ '</b> <br>'+'<b> Array Size : '+str(row['Size of Array (in kW)'])+'</b> <br>'+'<b> Installer :'+row['Installer']+'</b>', script=True)
-#                 popup = folium.Popup(test, max_width=2650)
-#                 folium.Marker(location=[row["Latitude"], row["Longitude"]],popup=popup,icon=folium.Icon(color='red',icon='university', prefix='fa')).add_to(group_1)
-#             else:
-#                 test = folium.Html('<h5>'+row["Recipient"]+'</h5>'+'<p>('+row['Type']+')</p>'+'<b> Project Grant : $' +str(row['Value of grant'])+ '</b> <br>'+'<b> Array Size : '+str(row['Size of Array (in kW)'])+'</b>', script=True)
-#                 popup = folium.Popup(test, max_width=2650)
-#                 folium.Marker(location=[row["Latitude"], row["Longitude"]],popup=popup,icon=folium.Icon(color='red',icon='university', prefix='fa')).add_to(group_1)
-
-    
-#     elif (row['Marker_type'] == 'Faith'):
-#         if(row['Installer'] != 'Unknown'):
-#             test = folium.Html('<h5>'+row["Recipient"]+'</h5>'+'<p>('+row['Type']+')</p>'+'<b> Project Grant : $' +str(row['Value of grant'])+ '</b> <br>'+'<b> Array Size : '+str(row['Size of Array (in kW)'])+'</b> <br>'+'<b> Installer :'+row['Installer']+'</b>', script=True)
-#             popup = folium.Popup(test, max_width=2650)
-#             folium.Marker(location=[row["Latitude"], row["Longitude"]],popup=popup,icon=folium.Icon(color='blue',icon='church', prefix='fa')).add_to(group_2)
-#         else:
-#             test = folium.Html('<h5>'+row["Recipient"]+'</h5>'+'<p>('+row['Type']+')</p>'+'<b> Project Grant : $' +str(row['Value of grant'])+ '</b> <br>'+'<b> Array Size : '+str(row['Size of Array (in kW)'])+'</b>', script=True)
-#             popup = folium.Popup(test, max_width=2650)
-#             folium.Marker(location=[row["Latitude"], row["Longitude"]],popup=popup,icon=folium.Icon(color='blue',icon='church', prefix='fa')).add_to(group_2)
-
-#     elif(row['Marker_type'] == 'Human Services'):
-#         if(row['Installer'] != 'Unknown'):
-#             test = folium.Html('<h5>'+row["Recipient"]+'</h5>'+'<p>('+row['Type']+')</p>'+'<b> Project Grant : $' +str(row['Value of grant'])+ '</b> <br>'+'<b> Array Size : '+str(row['Size of Array (in kW)'])+'</b> <br>'+'<b> Installer :'+row['Installer']+'</b>', script=True)
-#             popup = folium.Popup(test, max_width=2650)
-#             folium.Marker(location=[row["Latitude"], row["Longitude"]],popup=popup,icon=folium.Icon(color='green',icon='building', prefix='fa')).add_to(group_3)
-#         else:
-#             test = folium.Html('<h5>'+row["Recipient"]+'</h5>'+'<p>('+row['Type']+')</p>'+'<b> Project Grant : $' +str(row['Value of grant'])+ '</b> <br>'+'<b> Array Size : '+str(row['Size of Array (in kW)'])+'</b>', script=True)
-#             popup = folium.Popup(test, max_width=2650)
-#             folium.Marker(location=[row["Latitude"], row["Longitude"]],popup=popup,icon=folium.Icon(color='green',icon='building', prefix='fa')).add_to(group_3)
-    
-#     else:
-#         if(row['Installer'] != 'Unknown'):
-#             test = folium.Html('<h5>'+row["Recipient"]+'</h5>'+'<p>('+row['Type']+')</p>'+'<b> Project Grant : $' +str(row['Value of grant'])+ '</b> <br>'+'<b> Array Size : '+str(row['Size of Array (in kW)'])+'</b> <br>'+'<b> Installer :'+row['Installer']+'</b>', script=True)
-#             popup = folium.Popup(test, max_width=2650)
-#             folium.Marker(location=[row["Latitude"], row["Longitude"]],popup=popup,icon=folium.Icon(color='purple',icon='flag', prefix='fa')).add_to(group_4)
-#         else:
-#             test = folium.Html('<h5>'+row["Recipient"]+'</h5>'+'<p>('+row['Type']+')</p>'+'<b> Project Grant : $' +str(row['Value of grant'])+ '</b> <br>'+'<b> Array Size : '+str(row['Size of Array (in kW)'])+'</b>', script=True)
-#             popup = folium.Popup(test, max_width=2650)
-#             folium.Marker(location=[row["Latitude"], row["Longitude"]],popup=popup,icon=folium.Icon(color='purple',icon='flag', prefix='fa')).add_to(group_4)
-
-# folium.LayerControl().add_to(usa)
-# Geocoder().add_to(usa)
-# usa.save('usa.html')
-
-# # for idx, row in data.iterrows():
-# #     folium.CircleMarker(location=[row["Latitude"], row["Longitude"]],radius=25,fill=True,popup=row["Place Name"]).add_to(usa)
-# usa.save('usa.html')
