39d8550dd95bee8f1aca29d6b31b925acb4a3312
Anushka Vuppala <anushkavuppala@gwu.edu>
2023-11-18 12:52:05 -0500

    UI work

--- UI/app.py
+++ UI/app.py
@@ -1,88 +1,23 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import mapping
+import address_conversion
 
-# Paste your function here
 
-def convert_address_to_coords(original_file_name):
-    df = pd.read_excel(original_file_name,header=0)
-
-    addresses_converted =0
-    total_addresses=0
-    errorcount_geoapify=0
-    import requests
-    df["Latitude"]=""
-    df["Longitude"]=""
-    df["Incomplete Address"]=""
-    lats=list()
-    longs=list()
-    incomplete_address_flag=list()
-    # Replace YOUR_API_KEY with your actual API key. Sign up and get an API key on https://www.geoapify.com/ 
-    API_KEY = "d7962919004849b8828dd7ab4ddb2bfe"
-    for i in df["Site Address"]:
-        total_addresses+=1
-    # Define the address to geocode
-        address = i
-        try:
-            zip_code = int(address.split()[-1])
-            incomplete_address_flag.append("FALSE")
-        except:
-            incomplete_address_flag.append("TRUE")
-        
-        # Build the API URL
-        url = f"https://api.geoapify.com/v1/geocode/search?text={address}&limit=1&apiKey={API_KEY}"
-
-        # Send the API request and get the response
-        response = requests.get(url)
-        print("Address:",address)
-        # Check the response status code
-        if response.status_code == 200:
-            try:
-                # Parse the JSON data from the response
-                data = response.json()
-
-                # Extract the first result from the data
-                result = data["features"][0]
-
-                # Extract the latitude and longitude of the result
-                latitude = result["geometry"]["coordinates"][1]
-                longitude = result["geometry"]["coordinates"][0]
-
-                print(f"Latitude: {latitude}, Longitude: {longitude}")
-                
-                lats.append(latitude)
-                longs.append(longitude)
-                addresses_converted+=1
-            except Exception as e:
-                errorcount_geoapify+=1
-                print(f"Error processing address {address}: {e}")
-        else:
-            lats.append(np.nan)
-            longs.append(np.nan)
-            errorcount_geoapify+=1
-            print(f"Request failed with status code {response.status_code} for address {i}")
-            incomplete_address_flag.append("TRUE")         
-    print("Total errors = "+str(errorcount_geoapify))        
-
-    df["Latitude"]=lats
-    df["Longitude"]=longs
-    df["Incomplete Address"]=incomplete_address_flag
-    st.write("Number of total addresses to be converted: ",total_addresses)
-    st.write("Number of total addresses converted successfully: ",addresses_converted)
-    st.write("Number of total addresses that were incomplete (without zip codes): " + str(incomplete_address_flag.count(True)))
-    st.write("Number of total address conversions that failed: ",errorcount_geoapify)
-    return df
 
 # Streamlit app
 def main():
     st.title("Address to Coordinates Converter")
 
     uploaded_file = st.file_uploader("Upload Excel file", type=['xlsx'])
-
+    
     if uploaded_file is not None:
-        df = convert_address_to_coords(uploaded_file)
-
+        df = address_conversion.convert_address_to_coords(uploaded_file)
         st.write("Coordinates DataFrame:", df)
+        mapping.main(df)
+
+        
 
         if st.button("Save as CSV"):
             df.to_csv("streamlit-test-coordinates_output.csv", index=False)
